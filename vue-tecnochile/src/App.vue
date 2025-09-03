21d7bafac0819784fafec26904657237652ff244
mt-dafonte <mteresa.delafuente.c@gmail.com>
2025-09-03 17:12:31 -0400

    Agrega sección Nosotros, footer y botón de Whatsapp

--- vue-tecnochile/src/App.vue
+++ vue-tecnochile/src/App.vue
@@ -4,6 +4,11 @@ import productsData from './data/products.json';
 import NavBar from './components/NavBar.vue';
 import WelcomeTitle from './components/WelcomeTitle.vue';
 import FeaturedProductsCard from './components/FeaturedProductsCard.vue';
+import AboutUs from './components/AboutUs.vue';
+import Footer from './components/Footer.vue';
+import Whatsapp from './components/Whatsapp.vue';
+
+
 
 const featuredProducts = ref([]);
 // Accede al array 'productos' dentro del JSON
@@ -16,6 +21,9 @@ featuredProducts.value = productsData.productos.slice(0, 6);
 <NavBar />
 <WelcomeTitle />
 <FeaturedProductsCard :products="featuredProducts" />
+<AboutUs />
+<Footer />
+<Whatsapp />
 </template>
 
 <style scoped>
---
+++ vue-tecnochile/src/components/Footer.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer class="mt-auto ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <h5>Ubicados en</h5>
+                    <p>Avenida Providencia 2124, local 6-A.</p>
+                    <p>Santiago, Chile</p>
+                    <img class="img-fluid" style="width: 50px;" src="../assets/img/angellist-svgrepo-com.svg" alt="">
+                </div>
+                <div class="col-md-4 mb-3">
+                    <h5>Ir a link</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="index.html" class="text-decoration-none text-white">Inicio</a></li>
+                        <li><a href="products.html" class="text-decoration-none text-white">Productos</a></li>
+                        <li><a href="#aboutUs" class="text-decoration-none text-white">Sobre nosotros</a></li>
+                        <li><a href="#contact" class="text-decoration-none text-white">Contacto</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <h5>Síguenos</h5>
+                    <ul class="list-inline social-icons">
+                        <li class="list-inline-item"><a href="https://www.instagram.com/" class="text-white"><i
+                                    class="bi bi-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="https://www.facebook.com/" class="text-white"><i
+                                    class="bi bi-facebook"></i></a>
+                        </li>
+                        <li class="list-inline-item"><a href="https://www.linkedin.com/" class="text-white"><i
+                                    class="bi bi-linkedin"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <hr class="mb-4">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <p>&copy; 2025 <a href="">Grupo 6</a> Todos los derechos reservados.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</template>
+
+<style scoped>
+footer {
+  background-color: #1d1d1d;
+  color: #fff;
+  padding: 3rem 0;
+}
+
+.social-icons a {
+  font-size: 1.25rem;
+}
+</style>
